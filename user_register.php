dfaf69d4e160fe885a014aed0bb05743050b6d39
Eyorika <eyorikaict@gmail.com>
2024-03-17 23:11:48 +0300

    mod

--- user_register.php
+++ user_register.php
@@ -58,18 +58,18 @@ if(isset($_POST['submit'])){
 </head>
 <body>
 
-<div class="pre-loader">
+<!-- <div class="pre-loader">
 		<div class="pre-loader-box">
-			<div class="loader-logo"><img src="../vendors/images/deskapp-logo-svg.png" alt=""></div>
+			<div class="loader-logo"><img src="../images/adm.png" alt=""></div>
 			<div class='loader-progress' id="progress_div">
 				<div class='bar' id='bar1'></div>
 			</div>
 			<div class='percent' id='percent1'>0%</div>
 			<div class="loading-text">
-				Loading InspireTech ...
+				Loading Inspire Tech ...
 			</div>
 		</div>
-	</div>
+	</div>-->
    
 <?php include 'components/user_header.php'; ?>
 
